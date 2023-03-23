1361e0bab9c5aeb68f74755c785ebcd3796f3986
yanuarprayoga09 <yanuarprayoga@gmail.com>
2023-03-23 13:16:06 +0700

    kumpulan task codewars

---
+++ two to one.js
@@ -0,0 +1,8 @@
+function longest(s1,s2){
+    //  s1 = "xyaabbbccccdefww";
+    //  s2 = "xxxxyyyyabklmopq";
+
+     let arraySet= new Set(s2+s2);
+     return arraySet;
+}
+console.log(longest("xyaabbbccccdefww","xxxxyyyyabklmopq"))
