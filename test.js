8932a2963618dd301fcb3533fe8bdc1d457ffe25
yanuarprayoga09 <yanuarprayoga@gmail.com>
2023-03-23 11:23:16 +0700

    first commit

---
+++ test.js
@@ -0,0 +1,13 @@
+ 
+ let str = "halo"
+ 
+ for(let i = 0;i<str.length;i++){
+      let ch = str.charAt(i);
+    //   if(ch==vowels[i]){
+    //     return 1
+    //   }else{
+    //     return 0;
+    //   }
+    console.log(ch)
+    }
+    
---
+++ vowels.js
@@ -0,0 +1,23 @@
+function getCount(str) {
+  
+    //jika setiap ketemu huruf vokal maka +1
+     //jika setiap ketemu huruf vokal maka +1
+   let vowels=["a","i","u","e","o"]
+    let strSplit = str.split("");
+    
+    // perulangan untuk menampilkan vowels
+    for(let i = 0 ; i<vowels.length;i++){
+        let sama=0;
+        for(let a = 0;a<strSplit.length;a++){
+            console.log(strSplit[a]);
+            if(strSplit.includes(vowels[i])){
+                sama=1;
+               return sama++
+            }else{
+                return 0;
+            }
+        }
+    }
+    
+  }
+  console.log(getCount("saya ingin makan nasi padangq   "))
